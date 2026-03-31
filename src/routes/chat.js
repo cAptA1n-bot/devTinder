5a05e04099d57f862d76f928dba95c81d701d417
Prince Yadav <prince1bot001@gmail.com>
2026-03-31 23:37:27 +0530

    fix: sent userId on messageReceived event

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Chat = require("../models/chat");
 const { userAuth } = require("../middleware/auth");
 
 const chatRouter = express.Router();
@@ -7,8 +8,14 @@ chatRouter.get("/chats/:targetUserId", userAuth, async(req, res) => {
     try{
         const userId = req.user;
         const targetUserId = req.params.targetUserId;
-        const chat = await Chat.findOne({participants: {$all: [userId, targetUserId]}})
-        console.log(chat);
+        const chat = await Chat.findOne({participants: {$all: [userId, targetUserId]}}).populate("messages.senderId", "firstName lastName"); 
+        if(!chat){
+            chat = new Chat({
+                participants: [userId, targetUserId],
+                messages: []
+            })
+            await chat.save();
+        }
         res.status(200).json({data: chat});
     }
     catch(err){
