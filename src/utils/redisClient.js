874d507e2b3067d1e1ee9005d8a713fc7c58f336
Prince Yadav <prince1bot001@gmail.com>
2026-04-13 17:47:50 +0530

    added redis cloud

--- src/utils/redisClient.js
+++ src/utils/redisClient.js
@@ -1,6 +1,13 @@
 const redis = require('redis');
 
-const client = redis.createClient();
+const client = redis.createClient({
+    username: 'default',
+    password: process.env.REDIS_PASS,
+    socket: {
+        host: process.env.REDIS_URL,
+        port: 10873
+    }
+});
 
 client.on('error', (err) => {
     console.error('Redis Client Error', err);
